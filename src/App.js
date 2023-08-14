467722dc90ed3ef88ae21d6e1f1b2de5c6195bf1
Inmeperial <quique_12_04@hotmail.com>
2023-08-14 02:52:12 -0300

    Added new example for functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import TextField from '@mui/material/TextField';
+class RenderFunction extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            answer: "Answer"
+        }
+        this.getSum = this.getSum.bind(this)
+    }
+ 
+    getSum() {
+        let x = parseInt(document
+            .getElementById("elementA").value);
+ 
+        let y = parseInt(document
+            .getElementById("elementB").value);
+ 
+        console.log(x + y)
+        this.setState({
+            answer: x + y
+        })
+    }
+ 
+    render() {
+        return (
+            <center>
+                <div className="App"> 
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <p>
+                            Edit <code>src/App.js</code> and save to reload.
+                        </p>
+                        <a
+                            className="App-link"
+                            href="https://reactjs.org"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                        Learn React
+                        </a>
+                    </header>
+                    MyButton();
+                </div>
+                <h1>
+                    We will be calling sum
+                    function from render
+                </h1>
+                <div>
+                    <TextField id="elementA"
+                        variant="outlined" />  
+ 
+                    <TextField id="elementB"
+                        variant="outlined" />
+                    <br></br>
+                    <br></br>
+ 
+                    <button onClick={this.getSum}
+                        className="btn btn-primary">
+                        Get Sum
+                    </button>
+                    <br></br>
+                    <br></br>
+                    <TextField id="elementC" disabled
+                        variant="outlined"
+                        value={this.state.answer} />
+                </div>
+            </center>
+        )
+    }
+}
+export default function App() {
+    return (
+        <div className="App">
+            <RenderFunction />
+        </div>
+    );
+}
+
+/*
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -18,6 +100,7 @@ function App() {
           Learn React
         </a>
       </header>
+      MyButton();
     </div>
   );
 }
@@ -31,3 +114,4 @@ function MyButton() {
 }
 
 export default App;
+*/
