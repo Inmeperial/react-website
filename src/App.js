0b03b1d85065d924e91c0748ea7e5dff9795199c
Inmeperial <quique_12_04@hotmail.com>
2023-08-15 01:17:09 -0300

    Testing for func and classes

--- src/App.js
+++ src/App.js
@@ -2,82 +2,93 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import TextField from '@mui/material/TextField';
+
+// https://react.dev/reference/react/Component
+    // We recommend defining components as functions instead of classes.
+
+
+
 class RenderFunction extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            answer: "Answer"
-        }
-        this.getSum = this.getSum.bind(this)
-    }
- 
-    getSum() {
-        let x = parseInt(document
-            .getElementById("elementA").value);
- 
-        let y = parseInt(document
-            .getElementById("elementB").value);
- 
-        console.log(x + y)
-        this.setState({
-            answer: x + y
-        })
-    }
- 
+    
     render() {
         return (
             <center>
                 <div>
-                    <h1>Welcome to my app</h1>
                     <MyButton />
                 </div>
                 <div className="App"> 
-                    <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <p>
-                            Edit <code>src/App.js</code> and save to reload.
-                        </p>
-                        <a
-                            className="App-link"
-                            href="https://reactjs.org"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                        Learn React
-                        </a>
-                    </header>
+                    <LogoWithText />
                 </div>
-                <h1>
-                    We will be calling sum
-                    function from render
-                </h1>
                 <div>
-                    <TextField id="elementA"
-                        variant="outlined" />  
- 
-                    <TextField id="elementB"
-                        variant="outlined" />
-                    <br></br>
-                    <br></br>
- 
-                    <button onClick={this.getSum}
-                        className="btn btn-primary">
-                        Get Sum
-                    </button>
-                    <br></br>
-                    <br></br>
-                    <TextField id="elementC" disabled
-                        variant="outlined"
-                        value={this.state.answer} />
+                    <SumClass/>
                 </div>
             </center>
-        )
+        );
     }
+
 }
 
-function MyButton() {
+getSum() {
+    let x = parseInt(document.getElementById("elementA").value);
+    let y = parseInt(document.getElementById("elementB").value);
+
+    console.log(x + y)
+    this.setState({ answer: x + y })
+}
+
+function SumClass () {
+    super();
+    this.state = { answer: "Answer" }
+    this.getSum = this.getSum.bind(this)
+    
     return (
-      <button>I'm a button</button>
+        <header>
+            <h1>
+                We will be calling sum
+                function from render
+            </h1>
+            <TextField id="elementA" variant="outlined" />  
+            <TextField id="elementB" variant="outlined" />
+            <br></br>
+            <br></br>
+
+            <button onClick={this.getSum} className="btn btn-primary">
+                Get Sum
+            </button>
+            <br></br>
+            <br></br>
+            <TextField id="elementC" disabled variant="outlined" value={this.state.answer} />
+        </header>
+    );
+}
+
+function LogoWithText()
+{
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+            Learn React
+            </a>
+        </header>
+    );
+}
+
+function MyButton()
+{
+    return (
+        <header>
+            <h1>Welcome to my app</h1>
+            <button>I'm a button</button>
+        </header>
     );
 }
 
